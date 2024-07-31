5b018b6c9c39f2722b044cf62b89b5a89ef14e52
Efrain <efrainlp1995@gmail.com>
2024-07-30 23:46:14 -0500

    avance

--- Assets/Scripts/rouletteObject.cs
+++ Assets/Scripts/rouletteObject.cs
@@ -6,27 +6,12 @@ public class rouletteObject : MonoBehaviour
 {
 
     int Degrees = 360;
-    bool B_isFinished = false;
-
-    private LineRenderer lineRenderer;
 
     public GameObject prefab;
     public Transform parentObject;
-    public Transform capsula;
-
-    public float angleDegrees = 0f; // Ángulo en grados
-    public float lineLength = 1f;   // Longitud de la línea
-    public Color lineColor = Color.red; // Color de la línea
-
-    public Vector3 vector3;
-
-
 
     public float rotationSpeed = 100f;
-    public int maxItems = 8;
     List<string> items;
-    List<Transform> Positions;
-    // Start is called before the first frame update
 
     void Start()
     {
@@ -37,14 +22,10 @@ public class rouletteObject : MonoBehaviour
             OnDrawGizmoss(grados);
             
         }
-        capsula.transform.parent=parentObject;
     }
 
-    // Update is called once per frame
     void Update()
-    {
-
-        
+    {        
         transform.Rotate(new Vector3(0,1,0),  Time.deltaTime * rotationSpeed);
     }
 
@@ -55,6 +36,11 @@ public class rouletteObject : MonoBehaviour
             "chocolate",
             "harina",
             "huevo",
+            "gelatina",
+            "gelatina",
+            "gelatina",
+            "gelatina",
+            "gelatina",
             "gelatina",
         };
         return names;
@@ -71,36 +57,9 @@ public class rouletteObject : MonoBehaviour
         Vector3 end = transform.position + direction * (transform.localScale.x/4);
 
         GameObject newObject =  Instantiate(prefab, end, Quaternion.identity);
-        newObject.transform.parent = capsula;
+        newObject.transform.parent = parentObject;
 
-        Quaternion additionalRotation = Quaternion.Euler(Angle,0,0); // Rotación adicional de 45 grados en el eje Y
+        Quaternion additionalRotation = Quaternion.Euler(Angle,0,0);
         newObject.transform.localRotation *= additionalRotation;
-
-        //newObject.transform.rotation = Quaternion.Euler(new Vector3(0, Angle, -90));
-        Debug.Log(newObject.transform.rotation);
-        // Positions.Add(newObject.transform);
-
-        // DrawLine(transform.position,end,Color.red);
-
     }
-
-    /* void DrawLine(Vector3 start, Vector3 end, Color color)
-    {
-        GameObject myLine = new GameObject();
-        myLine.transform.position = start;
-        myLine.AddComponent<LineRenderer>();
-        LineRenderer lr = myLine.GetComponent<LineRenderer>();
-        // lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
-        lr.startColor = color;
-        lr.endColor  = color;
-        lr.startWidth = 0.1f;
-        lr.endWidth = 0.1f;
-        lr.SetPosition(0, start);
-        lr.SetPosition(1, end);
-        Debug.Log(start);
-        Debug.Log(end);
-        Debug.Log("-------------->-.");
-        myLine.transform.parent = parentObject;
-    }
-    */
 }
